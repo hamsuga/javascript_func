9fdfde4f08a8d74518aa483822a19ae2b535f686
hamsuga <tyukodi.andras_2022b@bolyai-szakkozep.hu>
2025-10-10 08:55:34 +0200

    if es dolgok

---
+++ table_row/kitero.js
@@ -0,0 +1,15 @@
+const a ={
+
+}
+let szam = 12;
+if (szam > 5) {
+    a.name = "szia"
+}
+else{
+    a["name"] = "hello"
+}
+console.log(a);
+console.log(a["name"])
+if (a.age==undefined) { // ez a roviditese if(a.age) nem hamis de hamiskent kezeli
+       
+}
