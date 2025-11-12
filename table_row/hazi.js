87c525a7442d64b4d166d0df3793807495807d10
hamsuga <tyukodi.andras_2022b@bolyai-szakkozep.hu>
2025-11-12 13:40:41 +0100

    tokeletes htmltablazat

--- table_row/hazi.js
+++ table_row/hazi.js
@@ -1,5 +1,5 @@
 /**
- * @type {{nemz?:string,szerzo:string,mu:string}[]}
+ * @type {{nemz:string,szerzo:string,mu:string,szerzo1?:string,mu1?:string}[]}
  */
 const arr = [
     {
@@ -59,7 +59,7 @@ for (let b of arr) {
          * @type {HTMLTableCellElement} 
         */
         const valami = e.target //elkerjuk a tulajdonsagat es elrakja a valtozoba
-        valami.classList.add("marked") //css osztalybol a marked osztalyt hozza adjuk igy lesz a szin teal
+        valami.classList.add("marked") //css osztalybol(nem csak cssbol ) a marked osztalyt hozza adjuk igy lesz a szin teal
 
         const valami1 = valami.parentElement
         const tbody = valami1.parentElement
@@ -97,3 +97,100 @@ for (let b of arr) {
         td.rowSpan = '2'
     }
 }
+
+const valami3000 = document.getElementById("htmlform")
+valami3000.addEventListener("submit", function(e) {
+    /**
+         * @type {HTMLFormElement} 
+    */
+    e =  preventDefault()
+    const mehmehmeh = e.target 
+
+    /**
+     * @type {HTMLInputElement}
+     */
+    const nemzetiseg = mehmehmeh.querySelector("#nemzetiseg")
+    /**
+     * @type {string}
+     */
+    const nemzetisegerteke = nemzetiseg.value
+
+    /**
+     * @type {HTMLInputElement}
+     */
+    const szerzo = mehmehmeh.querySelector("#szerzo1")
+    /**
+     * @type {string}
+     */
+    const szerzoerteke = szerzo.value
+    /**
+     * @type {HTMLInputElement}
+     */
+    const mu = mehmehmeh.querySelector("#mu1")
+        /**
+     * @type {string}
+     */
+    const muerteke = mu.value
+
+    /**
+     * @type {HTMLInputElement}
+     */
+    const szerzo1 = mehmehmeh.querySelector("#szerzo2")
+        /**
+     * @type {string}
+     */
+    const szerzo1erteke = szerzo1.value
+
+    /**
+     * @type {HTMLInputElement}
+     */
+    const mu1= mehmehmeh.querySelector("#mu2")
+        /**
+     * @type {string}
+     */
+    const mu1erteke = mu1.value
+
+
+    /**
+     * @type {nemz:string,szerzo:string,mu:string,szerzo1?:string,mu1?:string}
+     */
+    const objektum = {}
+    objektum.nemz = nemzetisegerteke;
+    objektum.szerzo = szerzogerteke;
+    objektum.mu = muerteke;
+    objektum.szerzo1 = szerzo1egerteke;
+    objektum.mu1 = mu1erteke;
+
+
+    const tabla =document.getElementById("tablebody")
+
+    const tr = document.createElement("tr")
+    table.appendChild(tr)
+
+    const td = document.createElement("td")
+    td.innerText=nemzetisegerteke;
+    tr.appendChild(td)
+
+    const td1 = document.createElement("td")
+    td1.innerText=szerzo;
+    tr.appendChild(td1)
+
+    const td2 = document.createElement("td")
+    td2.innerText=mu;
+    tr.appendChild(td2)
+
+    if(szerzo1erteke != "" && mu1erteke != ""){
+        const tr2 = document.createElement('tr')
+        tbody.appendChild(tr2)
+
+        const td3 = document.createElement('td')
+        td3.innerText = szerzo1erteke
+        tr2.appendChild(td3)
+
+        const td4 = document.createElement('td')
+        td4.innerText = mu1erteke
+        tr2.appendChild(td4)
+
+        td.rowSpan = '2'
+    }
+})
